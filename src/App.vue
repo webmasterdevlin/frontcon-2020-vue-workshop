185961483bf60dbebcefef3c4e49475d5f0e7045
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-08-12 10:29:17 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <input v-model="message" />
     <h3 v-if="message.length">{{message}}</h3>
     <h3 v-else>No message entered</h3>
+    <ul>
+      <li v-for="m in messages" :key="m.id">{{m.id}} {{m.message}}</li>
+    </ul>
   </div>
 </template>
 
@@ -12,35 +15,25 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   data: () => ({
     message: "Vuejs Workshop",
+    messages: [
+      {
+        id: 1,
+        message: "Person A",
+      },
+      {
+        id: 2,
+        message: "Person B",
+      },
+      {
+        id: 3,
+        message: "Person C",
+      },
+    ],
   }),
   methods: {
     clearMessage() {
       this.message = "";
     },
-  },
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  created() {
-    console.log("created");
-  },
-  beforeMount() {
-    console.log("beforeMount");
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-  updated() {
-    console.log("updated");
-  },
-  beforeDestroy() {
-    console.log("beforeDestroy");
-  },
-  destroyed() {
-    console.log("destroyed");
   },
 };
 </script>
