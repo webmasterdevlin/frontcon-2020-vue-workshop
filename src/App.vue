531851d43ad0df3daab91672ceab441ee4b2f3af
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-08-12 11:49:53 +0300

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,22 @@
 <template>
-  <div @click="clearMessage">
-    <input v-model="message" />
-    <h3 v-if="message.length">{{message}}</h3>
-    <h3 v-else>No message entered</h3>
-
-    <h3 v-if="!messages.length">Loading..</h3>
-    <ul v-else>
-      <li v-for="m in messages" :key="m.id">{{m.id}} {{m.message}}</li>
-    </ul>
+  <div>
+    <HeaderNav></HeaderNav>
+    <div class="container">
+      <div class="my-5">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderNav from "./shared/components/Navbar";
 
 export default {
-  data: () => ({
-    message: "Vuejs Workshop",
-    messages: [],
-  }),
-  methods: {
-    clearMessage() {
-      this.message = "";
-    },
-  },
-  async mounted() {
-    // HTTP REquests
-    // response = await getMessagesService()
-    this.messages = response;
+  name: "App",
+
+  components: {
+    HeaderNav,
   },
 };
 </script>
--- src/heroes/views/Heroes.vue
+++ src/heroes/views/Heroes.vue
@@ -2,12 +2,12 @@
   <div class="container-fluid">
     <h1>Heroes Works!</h1>
     <section>
-      <div v-if="heroes.length > 0">
-        <div class="card mt-3" style="width: auto;" v-for="hero in heroes" :key="hero.id">
+      <div>
+        <div class="card mt-3" style="width: auto;">
           <div class="card-header">
-            <h3 class="card-title">{{ hero.firstName }} {{ hero.lastName }}</h3>
-            <h5 class="card-subtitle mb-2 text-muted">{{ hero.house }}</h5>
-            <p class="card-text">{{ hero.knownAs }}</p>
+            <h3 class="card-title">hero.firstName hero.lastName</h3>
+            <h5 class="card-subtitle mb-2 text-muted">hero.house</h5>
+            <p class="card-text">hero.knownAs</p>
           </div>
         </div>
       </div>
@@ -16,27 +16,11 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-
 export default {
   name: "Heroes",
 
   setup() {
     return {};
-  },
-  
-  computed: {
-    ...mapGetters("hero", {
-      heroes: "heroes",
-      isLoading: "isLoading",
-      error: "error",
-    }),
-  },
-  methods: {
-    ...mapActions("hero", ["getHeroesAction"]),
-  },
-  mounted() {
-    this.getHeroesAction();
   },
 };
 </script>
