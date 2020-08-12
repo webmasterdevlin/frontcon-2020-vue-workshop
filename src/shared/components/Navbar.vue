9ddd5373a10144b36ffd198ecabd3b2f4738be35
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-08-12 11:32:57 +0300

    updated

---
+++ src/shared/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-start">
+    <span class="navbar-brand">LOGO</span>
+
+    <ul class="navbar-nav d-flex">
+      <li class="nav-item">
+        <router-link to="/" exact class="nav-link">Heroes</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script></script>
+
+<style scoped></style>
