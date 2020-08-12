dc2ee96f251075849cbc5a66a68d678a25c9838e
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-08-12 11:43:36 +0300

    updated

--- src/heroes/views/Heroes.vue
+++ src/heroes/views/Heroes.vue
@@ -24,6 +24,7 @@ export default {
   setup() {
     return {};
   },
+  
   computed: {
     ...mapGetters("hero", {
       heroes: "heroes",
---
+++ src/store/hero/state.js
@@ -0,0 +1,13 @@
+const state = {
+  heroes: [],
+  hero: {
+    id: "",
+    firstName: "",
+    lastName: "",
+    house: "",
+    knownAs: "",
+  },
+  isLoading: false,
+};
+
+export default state;
